60e7be6703c6f5a06c3e4aa8b5ab0ea7adeb4d38
Alumno ULSA <alumno@D514.local>
2022-11-04 08:51:17 -0700

    avance

---
+++ ProyectoParcial3/Materia.swift
@@ -0,0 +1,19 @@
+//
+//  Materia.swift
+//  ProyectoParcial3
+//
+//  Created by Alumno ULSA on 04/11/22.
+//
+
+class Materia {
+    var materia : String
+    var horario : String
+    var faltas : String
+    
+    init(materia:String, horario: String, faltas: String){
+        self.materia = materia
+        self.horario = horario
+        self.faltas = faltas
+    }
+}
+
---
+++ ProyectoParcial3/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ProyectoParcial3
+//
+//  Created by Alumno ULSA on 04/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var materias : [Materia] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return materias.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 69
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaMateria") as! CeldaMateriaController
+                    celda.lblMateria.text = materias[indexPath.row].materia
+                    celda.lblHorario.text = materias[indexPath.row].horario
+                    celda.lblFaltas.text = materias[indexPath.row].faltas
+        
+                return celda
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        materias.append(Materia(materia:"Progamación", horario: "7:00 am - 9:00 am", faltas: "3" ))
+        materias.append(Materia(materia:"Animación", horario: "9:00 am - 11:00 am", faltas: "1"))
+        materias.append(Materia(materia:"Diosito", horario: "11:00 am - 12:00 pm", faltas: "0"))
+        
+        
+    }
+
+
+}
+
