fe20b3520c92ff5cc39fbf6213046fb3b2188ce4
Alumno ULSA <alumno@D514.local>
2022-11-14 08:57:24 -0700

    ya funcionaa

--- ProyectoParcial3/ViewController.swift
+++ ProyectoParcial3/ViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var tvMateria: UITableView!
+
     
+    @IBOutlet weak var tvMateria: UITableView!
     //var checked = false
     
     @IBOutlet weak var txtNombreAlumno: UILabel!
@@ -57,9 +58,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 lblTel1.text = alumno?.tel1
                 lblTel2.text = alumno?.tel2
         
-        materias.append(Materia(materia:"Progamación", horario: "7:00 am - 9:00 am", imagen: "fotito", faltas: "1", check: "check2"))
-        materias.append(Materia(materia:"Animación", horario: "9:00 am - 11:00 am", imagen: "fotito", faltas: "1", check: "check2"))
-        materias.append(Materia(materia:"Diosito", horario: "11:00 am - 12:00 pm", imagen: "fotito", faltas: "1", check: "check2"))
+        materias.append(Materia(materia:"Progamación",maestro: "Emiliano", horario: "7:00 am - 9:00 am", imagen: "fotito", faltas: "1", check: "check2", rate1: "enojado2", rate2: "triste2", rate3: "maso2", rate4: "feli2", rate5: "feliz2", evaluacion: 0))
+        materias.append(Materia(materia:"Animación",maestro: "Guillermo", horario: "9:00 am - 11:00 am", imagen: "fotito", faltas: "1", check: "check2",  rate1: "enojado2", rate2: "triste2", rate3: "maso2", rate4: "feli2", rate5: "feliz2",evaluacion: 0 ))
+        materias.append(Materia(materia:"Diosito",maestro: "Francisco", horario: "11:00 am - 12:00 pm", imagen: "fotito", faltas: "1", check: "check2",  rate1: "enojado2", rate2: "triste2", rate3: "maso2", rate4: "feli2", rate5: "feliz2", evaluacion: 0))
         
     }
     
@@ -73,6 +74,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         if segue.identifier == "actualizarMateria" {
            let destino = segue.destination as! DetallesMateria
             destino.materia=materias[tvMateria.indexPathForSelectedRow!.row]
+            destino.callBackActualizarAsistencia = actualizarAsistencia
         }
         
         }
@@ -83,6 +85,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             lblParentesco.text = alumno.parentesco
             lblTel1.text = alumno.tel1
             lblTel2.text = alumno.tel2
+        }
+    func actualizarAsistencia (materia: Materia) {
+        tvMateria.reloadData()
+            
         }
         
         
